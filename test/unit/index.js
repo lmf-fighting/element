7c60427f900ecad1dbf1d65e0a748f8bc97404ad
廖明凤 <mingfeng.liao@qdtech.ai>
2022-04-14 18:01:09 +0800

    update: 执行单元测试时指定测试文件，无需每次都进行全量测试

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,9 +1,13 @@
 require('babel-regenerator-runtime'); // add regenerator support for async await
 require('packages/theme-chalk/lib/index.css');
 
-// require all test files (files that ends with .spec.js)
-const testsContext = require.context('./specs', true, /\.spec$/);
-testsContext.keys().forEach(testsContext);
+if (process.env.TEST_FILE) {
+  require(`/specs/${ process.env.TEST_FILE }.spec.js`);
+} else {
+  // require all test files (files that ends with .spec.js)
+  const testsContext = require.context('./specs', true, /\.spec$/);
+  testsContext.keys().forEach(testsContext);
+}
 
 // require all src files except main.js for coverage.
 // you can also change this to match only the subset of files that
