2d74b4d08ae68b5de2007c93885640f8704579ad
廖明凤 <mingfeng.liao@qdtech.ai>
2022-04-14 15:52:56 +0800

    update：添加单元测试

--- test/unit/specs/test-component.spec.js
+++ test/unit/specs/test-component.spec.js
@@ -11,5 +11,33 @@ describe('TestComponent', () => {
     vm = createTest(TestComponent, true);
     expect(vm.$el).to.exist;
   });
+  it('size', () => {
+    vm = createTest(TestComponent, {
+      size: 'medium'
+    }, true);
+    let buttonElm = vm.$el;
+    expect(buttonElm.classList.contains('el-button--medium')).to.be.true;
+  });
+  it('plain', () => {
+    vm = createTest(TestComponent, {
+      plain: true
+    }, true);
+    let buttonElm = vm.$el;
+    expect(buttonElm.classList.contains('is-plain')).to.be.true;
+  });
+  it('round', () => {
+    vm = createTest(TestComponent, {
+      round: true
+    }, true);
+    let buttonElm = vm.$el;
+    expect(buttonElm.classList.contains('is-round')).to.be.true;
+  });
+  it('circle', () => {
+    vm = createTest(TestComponent, {
+      circle: true
+    }, true);
+    let buttonElm = vm.$el;
+    expect(buttonElm.classList.contains('is-circle')).to.be.true;
+  });
 });
 
